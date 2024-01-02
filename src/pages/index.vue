c002521f394f54cc90a462bc24e8ec660a855136
Volerde <Volerde@outlook.com>
2024-01-02 16:59:50 +0800

    first commit

---
+++ src/pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import HeaderBar from '../components/HeaderBar.vue'
+</script>
+
+<template>
+    <el-container>
+        <el-header>
+            <HeaderBar />
+        </el-header>
+    </el-container>
+</template>
+
+<style scoped></style>

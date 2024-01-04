32ed373cd316710fdfa1aaee8f7362f60436d95a
Volerde <Volerde@outlook.com>
2024-01-04 08:39:50 +0800

    移除无用代码

--- src/components/CarouselComponent.vue
+++ src/components/CarouselComponent.vue
@@ -15,17 +15,6 @@ defineProps<{ imageList: [] }>()
 </template>
 
 <style scoped>
-/* .el-carousel__item h3 {
-    color: #475669;
-    opacity: 1;
-    line-height: calc(30vh);
-    margin: 0;
-    text-align: center;
-} */
-
-/* .el-carousel {
-    min-width: calc(100vh);
-} */
 .fit-carousel {
     max-height: 500px;
 }
